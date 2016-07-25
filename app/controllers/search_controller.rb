ea25e85532a8ebc3b575e8e9cb83d92ef82f5f84
Nick Pisciotta <npisciotta@gmail.com>
2016-07-25 11:19:37 -0600

    Return all electric and petroleum fuel stations Enumerate all through the fuel stations to collect electric and petroleum fuel stations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    @search_results = FuelStation.search_by_location
+    @search_results = FuelStation.search_by_location(params["zip_code_search"])
   end
 
 end
--- app/models/fuel_station.rb
+++ app/models/fuel_station.rb
@@ -4,9 +4,11 @@ class FuelStation < ActiveRecord::Base
     @service ||= FuelStationService.new
   end
 
-  def self.search_by_location
-    byebug
-    search_results_hash = service.search_stations
+  def self.search_by_location(zip_code)
+    search_results_hash = service.search_stations(zip_code)["fuel_stations"]
+    all_electric_petroleum = search_results_hash.find_all do |fuel_station|
+      fuel_station["fuel_type_code"] == "ELEC" || "LPG"
+    end
   end
 
 
